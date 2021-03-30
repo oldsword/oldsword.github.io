1447c38f82561d53d97630ca6c8c1f578d0e585a
oldsword <81577541+oldsword@users.noreply.github.com>
2021-03-30 12:37:29 +0200

    Update exampleQIAT0.js

--- IAT/exampleQIAT0.js
+++ IAT/exampleQIAT0.js
@@ -2,18 +2,5 @@ define(['pipAPI', 'https://oldsword.github.io/IAT/quiat0.js'], function(APIConst
     var API = new APIConstructor();
 
 	return iatExtension({
-		{
-			fullscreen:false, //Should we show the task in full screen? A Qualtrics-only feature because in the usual Minno, we can go full-screen right at the beginning of the study.
-        
-			isTouch:false, //Set whether the task is on a touch device.
-			//Set the canvas of the task
-			canvas : {
-				maxWidth: 725,
-				proportions : 0.7,
-				background: '#ffffff',
-				borderWidth: 5,
-				canvasBackground: '#ffffff',
-				borderColor: 'lightblue'
-			}
 	});
 });
