b33948631424b08739e1179e59b8d4c976d66ccf
oldsword <81577541+oldsword@users.noreply.github.com>
2021-03-31 20:33:06 +0200

    Update exampleQIAT2.js

--- IAT/exampleQIAT2.js
+++ IAT/exampleQIAT2.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://oldsword.github.io/IAT/quiat10.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://oldsword.github.io/IAT/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
